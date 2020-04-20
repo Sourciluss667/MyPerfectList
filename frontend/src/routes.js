a996253e837eb9072f01db348c05b866318130fb
Komi EKPON <komi01234.ekp@gmail.com>
2020-04-20 09:11:56 +0200

    sigin up amelioration

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,15 @@
-import Login from "./components/Login";
-import SiginUp from "./components/SiginUp";
-import Welcome from "./components/Welcome";
-import Profile from "./components/Profile";
-import Page404 from "./components/Page404";
+import Login from './components/Login'
+import SiginUp from './components/SiginUp'
+import ConfirmationSiginUp from './components/ConfirmationSiginUp'
+import Welcome from './components/Welcome'
+import Profile from './components/Profile'
+import Page404 from './components/Page404'
 
 export const routes = [
   { path: '/', component: Welcome },
   { path: '/login', component: Login },
   { path: '/signup', component: SiginUp },
+  { path: '/confirmation_sigin', component: ConfirmationSiginUp },
   { path: '/profile/:username', component: Profile },
   { path: '/404', component: Page404 }
 ]
