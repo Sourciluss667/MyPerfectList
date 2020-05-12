38ccaaf0093a8b88b6d2028718a66958af6e6d60
Komi EKPON <komi01234.ekp@gmail.com>
2020-05-12 16:07:19 +0200

    fix conflit

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import Login from "./components/Login";
-import SiginUp from "./components/SiginUp";
-import Welcome from "./components/Welcome";
-import Profile from "./components/Profile";
-import Page404 from "./components/Page404";
-import Search from "./components/Search";
-import Settings from "./components/Settings";
-=======
 import Login from './components/Login'
 import SiginUp from './components/SiginUp'
 import ConfirmationSiginUp from './components/ConfirmationSiginUp'
@@ -21,7 +12,6 @@ import GoodReads from './components/colllections/GoodReads'
 import RateYourMusic from './components/colllections/RateYourMusic'
 import Others from './components/colllections/Others'
 import PasswordLost from './components/PasswordLost'
->>>>>>> 7c9838d0803dfb7664a620c007e090401cf5af90
 
 export const routes = [
   { path: '/', component: Welcome },
@@ -30,11 +20,6 @@ export const routes = [
   { path: '/confirmation_sigin', component: ConfirmationSiginUp },
   { path: '/profile/:username', component: Profile },
   { path: '/search', component: Search },
-<<<<<<< HEAD
-  { path: '/settings', component: Settings },
-  { path: '/404', component: Page404 }
-]
-=======
   { path: '/404', component: Page404 },
   { path: '/collections/MyAnimeList', component: MyAnimeList },
   { path: '/collections/IMDb', component: IMDb },
@@ -44,4 +29,3 @@ export const routes = [
   { path: '/collections/others', component: Others },
   { path: '/reset_password', component: PasswordLost }
 ]
->>>>>>> 7c9838d0803dfb7664a620c007e090401cf5af90
