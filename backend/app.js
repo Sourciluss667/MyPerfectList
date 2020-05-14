3328864cbfc5e4964d25abdd106f5941170265c3
Sourciluss667 <qcormand@gmail.com>
2020-05-14 19:40:14 +0200

    Base imdb history api

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const PostgresStore = require('./utils/PostgresStore')
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 var searchRouter = require('./routes/search')
+var imdbRouter = require('./routes/imdb')
 var cors = require('cors')
 var app = express()
 
@@ -37,5 +38,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/search', searchRouter)
+app.use('/imdb', imdbRouter)
 
 module.exports = app
