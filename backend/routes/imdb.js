e5b21a2e1407a3b1f8d8b95f89de862fea4a8464
Sourciluss667 <qcormand@gmail.com>
2020-05-16 17:25:34 +0200

    imdb backend

---
+++ backend/routes/imdb.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var router = express.Router()
+var superagent = require('superagent')
+
+/* GET home page. */
+router.get('/:token', async (req, res) => {
+  const token = req.params.token
+  const agent = superagent.agent()
+  
+  const result = await agent.get(`https://www.imdb.com/user/${token}/watchlist`)
+  
+  res.send(result.text)
+})
+
+module.exports = router
