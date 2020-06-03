e782aae070e52f7269c233ec0b28c2c96b888510
Phanio <komi01234.ekp@gmail.com>
2020-06-03 09:19:16 +0200

    fix conflt

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,13 +1,9 @@
 module.exports = {
   database: 'myPerfectList',
   host: 'localhost',
-<<<<<<< HEAD
-  password: 'be8ca9',
+  password : 'ps2020',
   // password: 'be8ca9',
   // password: 'quentin',
-=======
-  password: 'quentin',
->>>>>>> quentin
   port: 5432,
   user: 'postgres'
 }
