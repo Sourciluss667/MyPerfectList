abf7eaf58c01985e04c5edfe7c8cbd7827ac2161
Sourciluss667 <qcormand@gmail.com>
2020-07-01 09:15:07 +0200

    Fix bug

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -80,8 +80,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
-<style>
+<style scoped>
 * {
   box-sizing: border-box;
 }
@@ -97,8 +96,8 @@ export default {
 .column {
   flex: 40%;
   padding: 20px;
-  /*outline-style: solid;
-  outline-color:black;*/
+  outline-style: solid;
+  outline-color:black;
 }
 
 /* On screens that are 992px wide or less, go from four columns to two columns */
