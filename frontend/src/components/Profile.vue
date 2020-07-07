dae5c9d73e85a2ef275539f3ed702d9144dfd32d
Sourciluss667 <qcormand@gmail.com>
2020-07-07 15:45:21 +0200

    Profile finition

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="page" style="position: relative; top: -90px;">
     <BreadCrumb />
+
+    <!-- MODALS -->
+
+    <!-- Avatar -->
+    <Modal v-if="showModal.avatar" @close="showModal.avatar = false" @submit="editAvatar">
+      <h3 slot="header" class="title">Avatar</h3>
+      <div slot="body">
+        <input class="input" type="text" name="avatar" id="avatar-input" placeholder="Use a link...">
+      </div>
+    </Modal>
+
+    <!-- Username -->
+    <Modal v-if="showModal.username" @close="showModal.username = false" @submit="editUsername">
+      <h3 slot="header" class="title">Username</h3>
+      <div slot="body">
+        <input class="input" type="text" name="username" id="username-input" :placeholder="userData.username">
+      </div>
+    </Modal>
+
+    <!-- Name -->
+    <Modal v-if="showModal.name" @close="showModal.name = false" @submit="editName">
+      <h3 slot="header" class="title">Name</h3>
+      <div slot="body">
+        <input class="input" type="text" name="name" id="name-input" :placeholder="userData.name">
+      </div>
+    </Modal>
+
+    <!-- END MODALS -->
+
+
     <div id="profile" class="columns">
       <div class="column is-one-quarter has-background-light">
         <div class="has-text-left" v-if="userData.id === $parent.user.id">
@@ -14,47 +44,51 @@
         <br />
         <figure class="image is-128x128 has-image-centered">
           <img class="is-rounded" :src="userData.avatar_url" />
-          <div class="icon icon-on-image is-size-1" v-if="editMode" @click="editAvatar">
+          <div class="icon icon-on-image is-size-1" v-if="editMode" @click="showModal.avatar = true">
             <i class="fas fa-pen"></i>
           </div>
         </figure>
         <br />
         <span class="title is-4" id="username">{{ userData.username }}</span>
-        <div class="icon" style="margin-right: 5px;" v-if="editMode" @click="editUsername">
+        <div class="icon" style="margin-right: 5px;" v-if="editMode" @click="showModal.username = true">
           <i class="fas fa-pen"></i>
         </div>
         <br />
         <span class="subtitle is-5" id="name">{{ userData.name }}</span>
-        <div class="icon" style="margin-right: 5px;" v-if="editMode" @click="editName">
+        <div class="icon" style="margin-right: 5px;" v-if="editMode" @click="showModal.name = true">
           <i class="fas fa-pen"></i>
         </div>
         <br />
+
         <div v-if="editMode">
-          <div class="icon" style="color: blue;">
-            <i class="fas fa-mars"></i>
+          <div class="icon" style="color: blue;"  @click="editGender('MALE')">
+            <i class="fas fa-mars icon-edit"></i>
           </div>
-          <div class="icon" style="color: pink;">
-            <i class="fas fa-venus"></i>
+          <div class="icon" style="color: pink;" @click="editGender('FEMALE')">
+            <i class="fas fa-venus icon-edit"></i>
           </div>
-          <div class="icon has-text-primary">
-            <i class="fas fa-venus-mars"></i>
+          <div class="icon has-text-primary" @click="editGender('OTHER')">
+            <i class="fas fa-venus-mars icon-edit"></i>
           </div>
         </div>
         <div v-else>
-          <div class="icon" style="color: blue;" v-if="userData.gender === 'MALE'">
-            <i class="fas fa-mars"></i>
+          <div v-if="userData.gender === 'MALE'">
+            <div class="icon" style="color: blue;">
+              <i class="fas fa-mars"></i>
+            </div>
           </div>
-          <div
-            class="icon"
-            style="color: pink;"
-            v-else-if="userData.gender === 'FEMALE'"
-          >
-            <i class="fas fa-venus"></i>
+          <div v-if="userData.gender === 'FEMALE'">
+            <div class="icon" style="color: pink;">
+              <i class="fas fa-venus"></i>
+            </div>
           </div>
-          <div class="icon has-text-primary" v-else-if="userData.gender === 'OTHER'">
-            <i class="fas fa-venus-mars"></i>
+          <div v-if="userData.gender === 'OTHER'">
+            <div class="icon has-text-primary">
+              <i class="fas fa-venus-mars"></i>
+            </div>
           </div>
         </div>
+
         <br />
         <p>
           <span class="has-text-weight-bold">Bio :</span>
@@ -62,9 +96,11 @@
           inventore quae placeat dolorum a voluptates nemo quaerat laboriosam
           amet earum repellendus sit pariatur dolor voluptate libero, dicta nam
           ipsum eaque.
+          <!--
           <span class="icon" style="margin-right: 5px;" v-if="editMode" @click="editBio">
             <i class="fas fa-pen"></i>
           </span>
+          -->
         </p>
         <br />
         <span>
@@ -117,8 +153,10 @@
 
 <script>
 import { getUserByUsername } from "../services/users";
-import { changeAvatarUrl, changeName, changeUsername } from "../services/users";
+import { changeAvatarUrl, changeName, changeUsername, changeGender } from "../services/users";
 import BreadCrumb from "./BreadCrumb";
+import Modal from './Modal'
+
 export default {
   name: "Profile",
   data() {
@@ -126,56 +164,52 @@ export default {
       editMode: false,
       // Contient id, name, username, email, birthdate, gender
       userData: {},
-      username: ''
+      username: '',
+      showModal: {avatar: false, username: false, name: false}
     };
   },
   components: {
     BreadCrumb,
+    Modal
   },
   async created() {
     await this.init();
   },
-  methods: { // TODO -------------------------------------------------------------------- EDIT AVATAR, EDIT GENDER
+  methods: { // EDIT GENDER
     edit () {
       this.editMode = !this.editMode;
     },
     editAvatar () {
-      const url = prompt('Avatar URL :', '')
-      if (url !== '') {
-        this.userData.avatar_url = url
-        // Save url
-        changeAvatarUrl(url)
+      const value = document.getElementById('avatar-input').value
+      if (value != '' && value.length > 2) {
+        this.userData.avatar_url = value
+        changeAvatarUrl(value)
       }
-    },
-    editGender () {
 
+      this.showModal.avatar = false
+    },
+    editGender (gender) {
+      this.userData.gender = gender
+      changeGender(gender)
     },
     editUsername () {
-      let e = document.getElementById('username').innerHTML
-      if (e != '<input class="input" type="text" name="username" id="username-input" placeholder="' + this.userData.username + '">') {
-        document.getElementById('username').innerHTML = '<input class="input" type="text" name="username" id="username-input" placeholder="' + this.userData.username + '">'
-      } else if (e === '<input class="input" type="text" name="username" id="username-input" placeholder="' + this.userData.username + '">') {
-        const value = document.getElementById('username-input').value
-        if (value != '' && value.length > 2) {
-          this.userData.username = document.getElementById('username-input').value
-          changeUsername(this.userData.username)
-          this.$root.$emit('changeUsername', this.userData.username)
-        }
-        document.getElementById('username').innerHTML = `${this.userData.username}`
+      const value = document.getElementById('username-input').value
+      if (value != '' && value.length > 2) {
+        this.userData.username = value
+        changeUsername(value)
+        this.$root.$emit('changeUsername', this.userData.username)
       }
+
+      this.showModal.username = false
     },
     editName () {
-      let e = document.getElementById('name').innerHTML
-      if (e != '<input class="input" type="text" name="name" id="name-input" placeholder="' + this.userData.name + '">') {
-        document.getElementById('name').innerHTML = '<input class="input" type="text" name="name" id="name-input" placeholder="' + this.userData.name + '">'
-      } else if (e === '<input class="input" type="text" name="name" id="name-input" placeholder="' + this.userData.name + '">') {
-        const value = document.getElementById('name-input').value
-        if (value != '' && value.length > 2) {
-          this.userData.name = document.getElementById('name-input').value
-          changeName(this.userData.name)
-        }
-        document.getElementById('name').innerHTML = `${this.userData.name}`
+      const value = document.getElementById('name-input').value
+      if (value != '' && value.length > 2) {
+        this.userData.name = value
+        changeName(value)
       }
+
+      this.showModal.name = false
     },
     editBio () {
       console.log('Not implemented')
@@ -230,7 +264,7 @@ export default {
       } else {
         // TEMPORAIRE, BESOIN DE RAJOUTER DATE D'INSCRIPTION
         this.userData.inscriptiondate = new Date(
-          this.userData.birthdate
+          this.userData.creation_date
         ).toLocaleDateString();
         // Format date from string to Date object
         this.userData.birthdate = new Date(
@@ -265,5 +299,13 @@ export default {
 
 .fa-pen {
   cursor: pointer;
+}
+
+.icon-edit {
+  cursor: pointer;
+}
+
+.icon-edit:hover {
+  color: red;
 }
 </style>
